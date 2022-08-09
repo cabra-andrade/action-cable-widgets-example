0ce1369c3428e0848de721e9e764e31b2b759fdf
Caio Andrade <candrade@infotech.com>
2022-08-08 19:24:41 -0500

    Basic partial and controller structure

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
   def home
+    @dashboard = OpenStruct.new(
+      gross_income: build_gross_income_widget
+    )
+  end
+
+  private
+
+  def build_gross_income_widget
+    OpenStruct.new(
+      title: "Gross Income",
+      controller: "gross-income"
+    )
   end
 end
